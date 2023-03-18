320c964f10f3318ec2787b25920a84999da59ff6
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-18 15:16:47 -0500

    added listener for when the server is running

--- server/server.js
+++ server/server.js
@@ -12,4 +12,40 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+
+// Middleware
 app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from Code Buddy',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt
+
+        const response = await openai.createCompletion({
+            model: "code-davinci-002",
+            prompt: `${prompt}`,
+            // Higher temperature value means the model will take greater risks. 
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1,
+            // Its not going to repeat similar sentences often. Higher the number the less likely it will respond the same way to the same question.
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status[500].send({ error })
+    }
+})
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
